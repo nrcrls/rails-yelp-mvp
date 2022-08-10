030053a301179a64a341269c23e7e973f41b2fef
Nikki Carlos <nikkirosecarlos@gmail.com>
2022-08-10 13:38:57 +0900

    finished yelm mvp crud

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,15 +1,17 @@
 class ReviewsController < ApplicationController
   def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
 
   def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
     if @review.save
       redirect_to restaurant_path(@restaurant)
     else
-      render :new, status: unprocessable_entity
+      render :new
     end
   end
 
